7494e1ee4a63225a8c6e2574550aeb4b99b85538
Kinga <kiss.kinga41@gmail.com>
2022-10-04 17:29:05 +0200

    Wrote Fu class

--- RokaMoka/RokaMoka/Fu.cs
+++ RokaMoka/RokaMoka/Fu.cs
@@ -8,5 +8,41 @@ namespace RokaMoka
 {
     class Fu
     {
+        private int tp;
+        internal int TP 
+        { 
+            get => tp; 
+            private set
+            {
+                if (value >= 0 && value <= 2)
+                {
+                    tp = value;
+                }
+                else
+                {
+                    tp = 0;
+                }
+            }
+        }
+        public Fu(int kezdoTP)
+        {
+            TP = kezdoTP;
+        }
+
+        internal void No()
+        {
+            if (TP != 2)
+            {
+                TP++;
+            }
+        }
+
+        internal void Csokken()
+        {
+            if (TP != 0)
+            {
+                TP--;
+            }
+        }
     }
 }
